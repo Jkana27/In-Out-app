823f95ba87a2560c8948d5a69e2e350b84a2fb41
JacobKana <jacob_kana@yahoo.com>
2022-02-05 01:54:38 -0600

    Add project files.

---
+++ In Out app/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace In_Out_app
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ In Out app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace In_Out_app
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
